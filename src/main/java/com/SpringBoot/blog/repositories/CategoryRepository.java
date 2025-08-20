8e27b62f94cf735ad1c652526b537270725e08ae
BuildTools <melihselimog_2007@hotmail.com>
2025-08-20 23:16:23 +0300

    Repo annotation added

--- src/main/java/com/SpringBoot/blog/repositories/CategoryRepository.java
+++ src/main/java/com/SpringBoot/blog/repositories/CategoryRepository.java
@@ -3,9 +3,11 @@ package com.SpringBoot.blog.repositories;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.SpringBoot.blog.domain.entities.Category;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, UUID> {
     
 
--- src/main/java/com/SpringBoot/blog/repositories/TagRepository.java
+++ src/main/java/com/SpringBoot/blog/repositories/TagRepository.java
@@ -3,9 +3,11 @@ package com.SpringBoot.blog.repositories;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.SpringBoot.blog.domain.entities.Tag;
 
+@Repository
 public interface TagRepository extends JpaRepository<Tag, UUID> {
     
     
