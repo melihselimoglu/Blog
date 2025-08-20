1d09f8c95078fb6edabf0e52ca3b57c2652741c6
BuildTools <melihselimog_2007@hotmail.com>
2025-08-20 23:13:42 +0300

    UserRepo saved

--- src/main/java/com/SpringBoot/blog/repositories/UserRepository.java
+++ src/main/java/com/SpringBoot/blog/repositories/UserRepository.java
@@ -1,5 +1,14 @@
 package com.SpringBoot.blog.repositories;
 
-public interface UserRepository {
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.SpringBoot.blog.domain.entities.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+
     
 }
