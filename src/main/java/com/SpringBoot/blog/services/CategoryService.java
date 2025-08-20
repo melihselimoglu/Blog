17223396960bfccc9f88869733f8fac36a0b3ea0
BuildTools <melihselimog_2007@hotmail.com>
2025-08-20 23:47:02 +0300

    CategoryService created

---
+++ src/main/java/com/SpringBoot/blog/services/CategoryService.java
@@ -0,0 +1,10 @@
+package com.SpringBoot.blog.services;
+
+import java.util.List;
+
+import com.SpringBoot.blog.domain.entities.Category;
+
+public interface CategoryService {
+    List<Category>  listCategories();
+    
+}
