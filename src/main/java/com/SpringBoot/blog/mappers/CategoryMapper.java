b34a890f5a9068c078ed5d58c19f939f07e2b617
BuildTools <melihselimog_2007@hotmail.com>
2025-08-21 00:03:37 +0300

    CategoryMapper created

---
+++ src/main/java/com/SpringBoot/blog/mappers/CategoryMapper.java
@@ -0,0 +1,30 @@
+package com.SpringBoot.blog.mappers;
+
+import java.util.List;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+import org.mapstruct.ReportingPolicy;
+
+import com.SpringBoot.blog.domain.PostStatus;
+import com.SpringBoot.blog.domain.dtos.CategoryDto;
+import com.SpringBoot.blog.domain.entities.Category;
+import com.SpringBoot.blog.domain.entities.Post;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface CategoryMapper {
+    
+    @Mapping(target = "postCount", source = "posts", qualifiedByName = "calculatePostCount")
+    CategoryDto toDto(Category category);
+
+    @Named("calculatePostCount")
+    default long calculatePostCount(List<Post> posts) {
+        if(posts == null) {
+            return 0;
+        }
+        return posts.stream()
+                .filter(post -> PostStatus.PUBLISHED.equals(post.getStatus()))
+                .count();
+    }
+}
