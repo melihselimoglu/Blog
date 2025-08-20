15fd5859252c7d4a920a7ee0125c0f1836c41de3
BuildTools <melihselimog_2007@hotmail.com>
2025-08-20 23:06:02 +0300

    Category and post relation

--- src/main/java/com/SpringBoot/blog/domain/entities/Category.java
+++ src/main/java/com/SpringBoot/blog/domain/entities/Category.java
@@ -1,5 +1,7 @@
 package com.SpringBoot.blog.domain.entities;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import jakarta.persistence.Column;
@@ -7,6 +9,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -29,6 +32,9 @@ public class Category {
 
     @Column(nullable = false, unique = true)
     private String name;
+
+    @OneToMany(mappedBy = "category")
+    private List<Post> posts = new ArrayList<>();
 
     @Override
     public int hashCode() {
