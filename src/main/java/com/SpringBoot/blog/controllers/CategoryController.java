82b09b156f2dff6eb05d95f0b01cee2e0a31be81
BuildTools <melihselimog_2007@hotmail.com>
2025-08-21 00:03:56 +0300

    CategoryController created

--- src/main/java/com/SpringBoot/blog/controllers/CategoryController.java
+++ src/main/java/com/SpringBoot/blog/controllers/CategoryController.java
@@ -13,15 +13,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 
 import com.SpringBoot.blog.domain.dtos.CategoryDto;
+import com.SpringBoot.blog.services.CategoryService;
+import com.SpringBoot.blog.mappers.CategoryMapper;
 
 @RestController
 @RequestMapping(path = "/api/v1/categories")
 @RequiredArgsConstructor
 public class CategoryController {
     
+    private final CategoryService categoryService;
+    private final CategoryMapper categoryMapper;
+
     @GetMapping
     public ResponseEntity<List<CategoryDto>> listCategories() {
-        
+        List<CategoryDto> categories = categoryService.listCategories()
+            .stream().map(categoryMapper::toDto)
+            .toList();
+        return ResponseEntity.ok(categories);
     }
     
 }
