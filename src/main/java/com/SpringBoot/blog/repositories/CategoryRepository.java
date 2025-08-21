9966dfb68d0e3e827f5c25496204b95cabf9fd6b
BuildTools <melihselimog_2007@hotmail.com>
2025-08-21 14:41:51 +0300

    existbyname to check if the category exist

--- src/main/java/com/SpringBoot/blog/repositories/CategoryRepository.java
+++ src/main/java/com/SpringBoot/blog/repositories/CategoryRepository.java
@@ -1,8 +1,10 @@
 package com.SpringBoot.blog.repositories;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.SpringBoot.blog.domain.entities.Category;
@@ -10,6 +12,8 @@ import com.SpringBoot.blog.domain.entities.Category;
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, UUID> {
     
-
+    @Query("SELECT c FROM Category c LEFT JOIN FETCH c.posts")
+    List<Category> findAllWithPostCount();
     
+    boolean existsByNameIgnoreCase(String name);
 }
