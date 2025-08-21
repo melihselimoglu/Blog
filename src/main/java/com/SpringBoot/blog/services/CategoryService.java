8b84a4331d96235a43d4d6cf288f1f201fddd639
BuildTools <melihselimog_2007@hotmail.com>
2025-08-21 14:40:30 +0300

    CreateCategory on service

--- src/main/java/com/SpringBoot/blog/services/CategoryService.java
+++ src/main/java/com/SpringBoot/blog/services/CategoryService.java
@@ -6,5 +6,5 @@ import com.SpringBoot.blog.domain.entities.Category;
 
 public interface CategoryService {
     List<Category>  listCategories();
-    
+    Category createCategory(Category category);
 }
--- src/main/java/com/SpringBoot/blog/services/impl/CategoryServiceImpl.java
+++ src/main/java/com/SpringBoot/blog/services/impl/CategoryServiceImpl.java
@@ -8,6 +8,7 @@ import com.SpringBoot.blog.domain.entities.Category;
 import com.SpringBoot.blog.repositories.CategoryRepository;
 import com.SpringBoot.blog.services.CategoryService;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -19,6 +20,15 @@ public class CategoryServiceImpl implements CategoryService {
     @Override
     public List<Category> listCategories() {
         return categoryRepository.findAllWithPostCount();
+    }
+
+    @Override
+    @Transactional
+    public Category createCategory(Category category) {
+        if(categoryRepository.existsByNameIgnoreCase(category.getName())) {
+            throw new IllegalArgumentException("Category with this name already exists"+ category.getName());
+        }
+        return categoryRepository.save(category);
     }
     
 }
