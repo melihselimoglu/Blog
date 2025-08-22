f9205c496a6d36709d180a85a1d043761d27e3d9
BuildTools <melihselimog_2007@hotmail.com>
2025-08-22 16:26:22 +0300

    AuthResponse and AuthController created

---
+++ src/main/java/com/SpringBoot/blog/controllers/AuthController.java
@@ -0,0 +1,34 @@
+package com.SpringBoot.blog.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.SpringBoot.blog.domain.dtos.AuthResponse;
+import com.SpringBoot.blog.domain.dtos.LoginRequest;
+import com.SpringBoot.blog.services.AuthenticationService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping(path = "/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthenticationService authenticationService;
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest loginRequest) {
+        UserDetails userDetails = authenticationService.authentication(
+            loginRequest.getEmail(), 
+            loginRequest.getPassword()
+        );
+        String tokenValue = authenticationService.generateToken(userDetails);
+        AuthResponse authResponse = AuthResponse.builder()
+            .token(tokenValue)
+            .expiresIn(86400) 
+            .build();
+            return ResponseEntity.ok(authResponse);
+    }
+
+}
