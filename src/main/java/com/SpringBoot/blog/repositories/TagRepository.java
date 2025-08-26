9f56ef52f19cb5b95cb466d6bc8a422385014390
BuildTools <melihselimog_2007@hotmail.com>
2025-08-26 16:06:38 +0300

    findByNameIn  method

--- src/main/java/com/SpringBoot/blog/repositories/TagRepository.java
+++ src/main/java/com/SpringBoot/blog/repositories/TagRepository.java
@@ -1,6 +1,7 @@
 package com.SpringBoot.blog.repositories;
 
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -14,4 +15,6 @@ public interface TagRepository extends JpaRepository<Tag, UUID> {
     
     @Query("SELECT t FROM Tag t LEFT JOIN FETCH t.posts")
     List<Tag> findAllWithPostCount();
+
+    List<Tag> findByNameIn(Set<String> names);
 }
