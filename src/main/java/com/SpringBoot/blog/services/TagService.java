29338ef47b272b7e54d16c6ea8a2b6de290ba6e2
BuildTools <melihselimog_2007@hotmail.com>
2025-08-26 16:05:00 +0300

    createTags method

--- src/main/java/com/SpringBoot/blog/services/TagService.java
+++ src/main/java/com/SpringBoot/blog/services/TagService.java
@@ -1,9 +1,11 @@
 package com.SpringBoot.blog.services;
 
 import java.util.List;
+import java.util.Set;
 
 import com.SpringBoot.blog.domain.entities.Tag;
 
 public interface TagService {
     List<Tag> getTags();
+    List<Tag> createTags(Set<String> tagNames);
 }
