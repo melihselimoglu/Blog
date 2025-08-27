dd668d2f81365fd4600d97dd130207e6a7d25598
BuildTools <melihselimog_2007@hotmail.com>
2025-08-27 16:10:56 +0300

    deleteTag endpoint on controller

--- src/main/java/com/SpringBoot/blog/controllers/TagController.java
+++ src/main/java/com/SpringBoot/blog/controllers/TagController.java
@@ -11,11 +11,14 @@ import com.SpringBoot.blog.services.TagService;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -49,4 +52,9 @@ public class TagController {
             HttpStatus.CREATED);
     }
     
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTag(@PathVariable UUID id) {
+        tagService.deleteTag(id);
+        return ResponseEntity.noContent().build();
+    }
 }
