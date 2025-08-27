8d6218a1ce69069b8a9b64b136a2463e2ed659a5
BuildTools <melihselimog_2007@hotmail.com>
2025-08-27 16:07:19 +0300

    deleteTag method

--- src/main/java/com/SpringBoot/blog/services/TagService.java
+++ src/main/java/com/SpringBoot/blog/services/TagService.java
@@ -2,10 +2,12 @@ package com.SpringBoot.blog.services;
 
 import java.util.List;
 import java.util.Set;
+import java.util.UUID;
 
 import com.SpringBoot.blog.domain.entities.Tag;
 
 public interface TagService {
     List<Tag> getTags();
     List<Tag> createTags(Set<String> tagNames);
+    void deleteTag(UUID id);
 }
