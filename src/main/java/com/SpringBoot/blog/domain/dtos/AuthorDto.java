8ab39daab9ebe44794d486b348482f78dbc486a2
BuildTools <melihselimog_2007@hotmail.com>
2025-08-28 14:52:22 +0300

    AuthorDto created

---
+++ src/main/java/com/SpringBoot/blog/domain/dtos/AuthorDto.java
@@ -0,0 +1,17 @@
+package com.SpringBoot.blog.domain.dtos;
+
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AuthorDto {
+    private UUID id;
+    private String name;
+}
