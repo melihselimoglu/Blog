97b526b50ee6fd7938114cd789eb1226e9d38a77
BuildTools <melihselimog_2007@hotmail.com>
2025-08-30 16:09:08 +0300

    findAllByStatusAndCategoryAndTagsContaining method

--- src/main/java/com/SpringBoot/blog/repositories/PostRepository.java
+++ src/main/java/com/SpringBoot/blog/repositories/PostRepository.java
@@ -1,15 +1,19 @@
 package com.SpringBoot.blog.repositories;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.SpringBoot.blog.domain.PostStatus;
+import com.SpringBoot.blog.domain.entities.Category;
 import com.SpringBoot.blog.domain.entities.Post;
+import com.SpringBoot.blog.domain.entities.Tag;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, UUID> {
-    
+    List<Post> findAllByStatusAndCategoryAndTagsContaining(PostStatus status, Category category, Tag tag);
    
     
 }
