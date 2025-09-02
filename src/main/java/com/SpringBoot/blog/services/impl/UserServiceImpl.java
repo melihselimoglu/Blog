f349d1d81da0c1331109e6b9ce8b6cb5cc9bf636
BuildTools <melihselimog_2007@hotmail.com>
2025-09-02 19:51:27 +0300

    getUserById implemented

---
+++ src/main/java/com/SpringBoot/blog/services/impl/UserServiceImpl.java
@@ -0,0 +1,25 @@
+package com.SpringBoot.blog.services.impl;
+
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+
+import com.SpringBoot.blog.domain.entities.User;
+import com.SpringBoot.blog.repositories.UserRepository;
+import com.SpringBoot.blog.services.UserService;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public User getUserById(UUID id) {
+        return userRepository.findById(id)
+            .orElseThrow(() -> new RuntimeException("User not found with id: " + id));
+    }
+    
+}
