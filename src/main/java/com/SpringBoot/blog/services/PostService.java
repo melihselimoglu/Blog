092eb0753aa96b83c5d7e0b0e36d679a27ed67e1
BuildTools <melihselimog_2007@hotmail.com>
2025-09-02 19:54:31 +0300

    getDraftPosts method

--- src/main/java/com/SpringBoot/blog/services/PostService.java
+++ src/main/java/com/SpringBoot/blog/services/PostService.java
@@ -4,7 +4,9 @@ import java.util.List;
 import java.util.UUID;
 
 import com.SpringBoot.blog.domain.entities.Post;
+import com.SpringBoot.blog.domain.entities.User;
 
 public interface PostService {
     List<Post> getAllPosts(UUID categoryId, UUID tagId);
+    List<Post> getDraftPosts(User user);
 }
