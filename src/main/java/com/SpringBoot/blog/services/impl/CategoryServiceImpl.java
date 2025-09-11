715dca2bac972b38021cfe7f7c9550fb370e839c
BuildTools <melihselimog_2007@hotmail.com>
2025-09-11 21:35:33 +0300

    getCategoryById implemented

--- src/main/java/com/SpringBoot/blog/services/impl/CategoryServiceImpl.java
+++ src/main/java/com/SpringBoot/blog/services/impl/CategoryServiceImpl.java
@@ -10,6 +10,7 @@ import com.SpringBoot.blog.domain.entities.Category;
 import com.SpringBoot.blog.repositories.CategoryRepository;
 import com.SpringBoot.blog.services.CategoryService;
 
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
@@ -42,6 +43,12 @@ public class CategoryServiceImpl implements CategoryService {
               }  
             categoryRepository.deleteById(id);   
        }
+    }
+
+    @Override
+    public Category getCategoryById(UUID id) {
+        return categoryRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Category not found with id" + id));
     }
     
 }
